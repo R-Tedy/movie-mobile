8572265cd844b30f6fc2eaccfc67543c89a4aa86
Roland Stedy <stedyroland@gmail.com>
2025-04-05 03:48:27 +0300

    navigation bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,8 +7,6 @@ export default function Index() {
       className="flex-1 justify-center items-center"
     >
       <Text className="text-5xl text-blue-950 font-bold">Welcome</Text>
-      <Link href='/onboarding'>Onboarding</Link>
-      <Link href='/movie/avengers'>Avengers Movie</Link>
     </View>
   );
 }
