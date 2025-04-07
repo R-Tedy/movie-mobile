09afa7bb9557d1c7cf8e465a448c3f22f226838b
Roland Stedy <stedyroland@gmail.com>
2025-04-07 08:52:46 +0300

    trending movies section

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -27,8 +27,6 @@ const search = () => {
       if (searchQuery.trim()){
         await loadMovies()
 
-        if (movies?.length > 0 && movies?.[0])
-        await updateSearchCount(searchQuery, movies[0]);
       } else {
         reset()
       }
@@ -36,6 +34,11 @@ const search = () => {
 
     return () => clearTimeout(timeoutId);
   },[searchQuery])
+
+  useEffect(() => {
+    if (movies?.length > 0 && movies?.[0])
+      updateSearchCount(searchQuery, movies[0]);
+  }, [movies])
   return (
     <View className='flex-1 bg-primary'>
       <Image source={images.bg} className='flex-1 absolute w-full z-0'/>
